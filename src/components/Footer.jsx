0465a79bcbc9cf01dafe3dbba9bf7b4c5fb69981
QuangVu-dev <tqvu97@gmail.com>
2024-07-03 23:33:07 +0700

    update code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="py-5 bg-dark">
       <div className="container">
         <p className="m-0 text-center text-white">
-          Copyright © Your Website 2023
+          Copyright © Your Website 2019
         </p>
       </div>
     </div>
